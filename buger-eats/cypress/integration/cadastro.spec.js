55d410afe53dc615e284331acebf7a11e2a941c0
welquerjunio <welquerrodrigues.05@gmail.com>
2022-06-09 21:47:33 -0300

    validando a busca de CEP

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -10,11 +10,29 @@ describe('Cadastro', ()=>{
             nome: 'Fernando Papito',
             cpf: '00000000078',
             email:'papito@hotmail.com',
-            whatsapp:'11999999999'
+            whatsapp:'11999999999',
+            endereco:{
+                cep: '04534011',
+                rua: 'Rua Joaquim Floriano',
+                numero: '1000',
+                complemento: 'Ap 142',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP'
+            }
         }
         cy.get('input[name="name"]').type(entregador.nome)
         cy.get('input[name="cpf"]').type(entregador.cpf)
         cy.get('input[name="email"]').type(entregador.email)
         cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
     })
 })
