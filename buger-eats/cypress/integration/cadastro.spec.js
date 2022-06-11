98b20bb33d5b5d9c9710e1077faffd54301a1ef3
welquerjunio <welquerrodrigues.05@gmail.com>
2022-06-11 16:59:14 -0300

    Validando span de alert

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,6 +1,7 @@
 // Teste de Cadastro de entregador
 
 describe('Cadastro', ()=>{
+    //Caso de teste 1
     it('Usuário deve se tornar um entregador', ()=>{
         cy.viewport(1440, 900)
         cy.visit('https://buger-eats.vercel.app/')
@@ -49,5 +50,54 @@ describe('Cadastro', ()=>{
         //Valida o texto após o upload da CNH
         const expectedMessage = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
         cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+    })
+    //Caso de teste 2
+    it('CPF incorreto', ()=>{
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app/')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+            nome: 'Fernando Papito',
+            cpf: '000000000AA',
+            email:'papito@hotmail.com',
+            whatsapp:'11999999999',
+            endereco:{
+                cep: '04534011',
+                rua: 'Rua Joaquim Floriano',
+                numero: '1000',
+                complemento: 'Ap 142',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP'
+            },
+            metodo_entrega: 'Moto',
+            cnh:'cnh-digital.jpg'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+
+        cy.get('input[accept^="image"]').attachFile('/images/'+ entregador.cnh)
+
+        cy.get('form button[type="submit"]').click()
+
+        //Valida o CPF incorreto
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
     })
 })
