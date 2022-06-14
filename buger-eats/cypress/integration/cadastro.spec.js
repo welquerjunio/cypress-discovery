b5f7affe57ce318c8c352114cfdaf84e8cb2aa08
welquerjunio <welquerrodrigues.05@gmail.com>
2022-06-13 22:04:24 -0300

    refatorando e usando fixtures para massa de teste

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -3,55 +3,24 @@ import signup from '../pages/SignupPage'
 
 describe('Cadastro', ()=>{
 
+    beforeEach(function(){
+        cy.fixture('deliver').then((d)=>{
+            this.deliver = d
+        })
+    })
     //Caso de teste 1
-    it('Usuário deve se tornar um entregador', ()=>{
-        
-        var deliver = {
-            nome: 'Fernando Papito',
-            cpf: '00000000078',
-            email:'papito@hotmail.com',
-            whatsapp:'11999999999',
-            address:{
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Ap 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh:'cnh-digital.jpg'
-        }
-    
+    it('Usuário deve se tornar um entregador', function(){
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
          //Valida o texto após o upload da CNH
         const expectedMessage = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
         signup.modalContentShouldBe(expectedMessage)   
     })
     //Caso de teste 2
-    it('CPF incorreto', ()=>{
-
-        var deliver = {
-            nome: 'Fernando Papito',
-            cpf: '000000000AA',
-            email:'papito@hotmail.com',
-            whatsapp:'11999999999',
-            address:{
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Ap 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh:'cnh-digital.jpg'
-        }
-
+    it('CPF incorreto', function(){
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.cpf_inv)
         signup.submit()
         signup.alertMessageShouldBe('Oops! CPF inválido')
         
